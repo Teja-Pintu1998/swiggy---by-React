3e05d651fb99f6f75d798ea6669f342d64afc112
Teja-Pintu1998 <saitejagunjala@gmail.com>
2023-09-25 23:08:52 -0500

    episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent = React.createElement("h4", { id: "parent" },"helloreact");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
